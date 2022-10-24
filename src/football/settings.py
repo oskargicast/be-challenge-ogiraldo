2f25388bb642f0b6311f97797973dac31d26093e
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2022-10-23 22:55:55 -0500

    chore: avoid permissions

--- src/football/settings.py
+++ src/football/settings.py
@@ -132,7 +132,7 @@ DRF_PAGE_SIZE = 10
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
