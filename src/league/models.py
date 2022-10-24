c4ea62d5b4b2e724866a50cb6cef7b2b893d3df8
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2022-10-23 22:56:33 -0500

    feat: expose import action handle competition creation

--- src/league/models.py
+++ src/league/models.py
@@ -1,5 +1,13 @@
+from django.conf import settings
 from django.db import models
 from model_utils.models import TimeStampedModel
+from league.api_client import (
+    LeagueAPIClient,
+    CompetitionData,
+    TeamData,
+    CoachData,
+    PlayerData,
+)
 
 
 class Competition(TimeStampedModel):
@@ -21,6 +29,18 @@ class Competition(TimeStampedModel):
 
     class Meta:
         ordering = ['-created']
+
+    @classmethod
+    def retrieve_and_create(cls, league_code):
+        client = LeagueAPIClient(
+            api_token=settings.API_TOKEN,
+            league_code=league_code,
+        )
+        response = client.get_competitions()
+        if response.status_code != 200:
+            raise Exception(f'Could not retrieve league {league_code}')
+        data = CompetitionData(response.json()).simplify()
+        return Competition.objects.create(**data)
 
 
 class Team(TimeStampedModel):
@@ -49,12 +69,14 @@ class Team(TimeStampedModel):
     )
     competitions = models.ManyToManyField(
         Competition,
-        related_name='competitions',
+        related_name='teams',
     )
     coach = models.OneToOneField(
         'league.Coach',
         related_name='coach',
         on_delete=models.CASCADE,
+        blank=True,
+        null=True,
     )
 
     def __str__(self):
--- src/league/views.py
+++ src/league/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Competition
 
-# Create your views here.
+
+class LeagueViewSet(viewsets.GenericViewSet):
+    lookup_field = 'league_code'
+
+    @action(detail=True, methods=['post'], url_path='import')
+    def import_data(self, request, league_code=None):
+        Competition.retrieve_and_create(league_code)
+        # TODO: Create QuerySet manager in Team model that retrieves
+        # and creates teams, coachs and players.
+        return Response(
+            status=status.HTTP_201_CREATED,
+        )
