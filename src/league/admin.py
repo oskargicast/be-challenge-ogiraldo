b3d157bb4f8eb3a3a1a5605c1b42a80d6e36ba8a
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2022-10-23 22:08:42 -0500

    feat: create models and expose via admin

--- src/league/admin.py
+++ src/league/admin.py
@@ -1,3 +1,88 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'code',
+        'name',
+        'area',
+    ]
+    search_fields = [
+        'id',
+        'code',
+        'name',
+    ]
+
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 0
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [
+        PlayerInline,
+    ]
+    list_display = [
+        'id',
+        'api_id',
+        'name',
+        'short_name',
+        'address',
+        'tla',
+        'area',
+        'coach',
+    ]
+    search_fields = [
+        'id',
+        'api_id',
+        'name',
+        'short_name',
+    ]
+    raw_id_fields = [
+        'coach',
+    ]
+
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'api_id',
+        'name',
+        'birthday',
+        'nationality',
+    ]
+    search_fields = [
+        'id',
+        'api_id',
+        'name',
+    ]
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'api_id',
+        'name',
+        'team',
+        'position',
+        'birthday',
+        'nationality',
+    ]
+    search_fields = [
+        'id',
+        'api_id',
+        'name',
+    ]
+    list_filter = [
+        'position',
+    ]
+    raw_id_fields = [
+        'team',
+    ]
--- src/league/models.py
+++ src/league/models.py
@@ -1,3 +1,109 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Competition(TimeStampedModel):
+    code = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+    name = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+    area = models.CharField(
+        max_length=50,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.name} ({self.code})'
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Team(TimeStampedModel):
+    api_id = models.IntegerField(
+        unique=True,
+    )
+    name = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+    short_name = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+    address = models.CharField(
+        max_length=300,
+        blank=True,
+    )
+    tla = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+    area = models.CharField(
+        max_length=50,
+        blank=True,
+    )
+    competitions = models.ManyToManyField(
+        Competition,
+        related_name='competitions',
+    )
+    coach = models.OneToOneField(
+        'league.Coach',
+        related_name='coach',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.id}.{self.name} - {self.api_id}'
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Person(TimeStampedModel):
+    api_id = models.IntegerField(
+        unique=True,
+    )
+    name = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+    birthday = models.DateField(blank=True, null=True)
+    nationality = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.id}.{self.name} - {self.api_id}'
+
+    class Meta:
+        abstract = True
+
+
+class Coach(Person):
+    class Meta:
+        ordering = ['-created']
+
+
+class Player(Person):
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+        related_name='players',
+    )
+    position = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+
+
+    def __str__(self):
+        return f'{self.id}.{self.name} - {self.api_id}'
+
+    class Meta:
+        ordering = ['-created']
